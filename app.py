1f83eb36f8c37c88d3925f6a343a6c8a6fa1a8ac
NoAt228 <134864567+NoAt228@users.noreply.github.com>
2025-10-26 00:00:07 +0300

    Update hello route response message

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from ECS Fargate!"
+    return "Hello from ECS Fargate! cicd completed"
 @app.route("/heavy")
 def heavy():
     """

c059c50ee434d5e6564a0890b978a05544bba8bc
Руслан Викторович Д <shazom@mail.ru>
2025-12-10 13:54:21 +0300

    add files for project - 2

--- scripts/login_ecr.sh
+++ scripts/login_ecr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Получаем регион из метаданных инстанса
-AWS_REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
+AWS_REGION=eu-central-1
 
 # Получаем ID аккаунта
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+AWS_ACCOUNT_ID=480241654314
 
 # Логинимся в ECR, используя полученные данные
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
